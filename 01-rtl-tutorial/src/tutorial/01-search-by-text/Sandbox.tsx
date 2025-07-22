bb2d31173dba0932e0028785b290c5ea72cc91bb
Richard Walsh <richwalsh2@googlemail.com>
2025-07-22 08:53:51 +0100

    section 2 lesson 11

--- 01-rtl-tutorial/src/tutorial/01-search-by-text/Sandbox.tsx
+++ 01-rtl-tutorial/src/tutorial/01-search-by-text/Sandbox.tsx
@@ -1,4 +1,29 @@
+import { useEffect, useState } from 'react';
+
 const Sandbox = () => {
-  return <div>Sandbox</div>;
+  const [showMessage, setShowMessage] = useState(false);
+  const [showError, setShowError] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMessage(true);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div>
+      <h1>React testing library examples</h1>
+      <p>you can search me with regular expression : 123-456-7890</p>
+      {showError && <p>Error message</p>}
+      <ul>
+        <li>item 1</li>
+        <li>item 1</li>
+        <li>item 1</li>
+        <li>item 1</li>
+      </ul>
+      {showMessage && <p>Async message</p>}
+    </div>
+  );
 };
 export default Sandbox;
