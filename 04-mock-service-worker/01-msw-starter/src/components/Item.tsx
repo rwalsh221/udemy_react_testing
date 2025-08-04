8bad9e6101a6d7acbc9cdbfb46f9cac3646b5e5d
Richard Walsh <richwalsh2@googlemail.com>
2025-08-04 07:49:06 +0100

    section 5 lesson 41

---
+++ 04-mock-service-worker/01-msw-starter/src/components/Item.tsx
@@ -0,0 +1,37 @@
+import { type Post } from '../hooks/usePosts';
+
+type ItemProps = {
+  post: Post;
+  onLike: (postId: string) => Promise<void>;
+  onDelete: (postId: string) => Promise<void>;
+};
+
+const Item = ({ post, onLike, onDelete }: ItemProps) => {
+  return (
+    <article className="border p-4 rounded flex items-center justify-between">
+      <h3 className="test-lg">{post.title}</h3>
+      <div className="flex items-center gap-4">
+        <div className="flex items-center gap-2">
+          <button
+            onClick={() => {
+              onLike(post.id);
+            }}
+            className="px-3 py-1 bg-teal-500 text-white rounded"
+          >
+            👍 {post.likes}
+          </button>
+        </div>
+        <button
+          className="px-3 py-1 bg-gray-700 text-white rounded"
+          onClick={() => {
+            onDelete(post.id);
+          }}
+        >
+          Delete
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default Item;
---
+++ 04-mock-service-worker/01-msw-starter/src/components/List.tsx
@@ -0,0 +1,22 @@
+import Item from './Item';
+import { type Post } from '../hooks/usePosts';
+
+type ListProps = {
+  posts: Post[];
+  onLike: (postId: string) => Promise<void>;
+  onDelete: (postId: string) => Promise<void>;
+};
+
+const List = ({ posts, onDelete, onLike }: ListProps) => {
+  return (
+    <div className="space-y-4">
+      {posts.map((post) => {
+        return (
+          <Item key={post.id} post={post} onLike={onLike} onDelete={onDelete} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default List;
