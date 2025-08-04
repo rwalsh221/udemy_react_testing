82cc3e105d7ce91b068265a9fe23369a9612eff1
Richard Walsh <richwalsh2@googlemail.com>
2025-08-04 22:01:52 +0100

    section 5 lesson 42--15:08

---
+++ 04-mock-service-worker/01-msw-starter/src/__tests__/List.test.tsx
@@ -0,0 +1,21 @@
+import { render, screen } from '@testing-library/react';
+import userEvent, { type UserEvent } from '@testing-library/user-event';
+import List from '../components/List';
+import { type Post } from '../hooks/usePosts';
+
+const mockPosts: Post[] = [
+  { id: '1', title: 'post1', likes: 0 },
+  { id: '2', title: 'post2', likes: 2 },
+];
+
+const mockOnLike = vi.fn();
+const mockOnDelete = vi.fn();
+
+describe('List Component', () => {
+  test('renders correct number of articles', () => {
+    render(
+      <List posts={mockPosts} onDelete={mockOnDelete} onLike={mockOnLike} />
+    );
+    expect(screen.getAllByRole('article')).toHaveLength(2);
+  });
+});
