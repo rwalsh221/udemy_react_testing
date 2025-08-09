2358d80a140f8b5c24be2745dd8fbad2d716beb5
Richard Walsh <richwalsh2@googlemail.com>
2025-08-09 20:27:34 +0100

    section 6 lesson 50

--- 05-search-github-users/01-starter/src/App.tsx
+++ 05-search-github-users/01-starter/src/App.tsx
@@ -1,6 +1,14 @@
+import { useState } from 'react';
+import SearchForm from './components/form/SearchForm';
+import UserProfile from './components/user/UserProfile';
+
 const App = () => {
+  const [userName, setUserName] = useState('qunicylarson');
   return (
-    <h1 className='text-3xl font-bold p-8'>Search Github Users - Starter</h1>
+    <main className="mx-auto max-w-6xl px-8 py-20">
+      <SearchForm userName={userName} setUserName={setUserName} />
+      <UserProfile userName={userName} />
+    </main>
   );
 };
 export default App;
---
+++ 05-search-github-users/01-starter/src/components/form/SearchForm.tsx
@@ -0,0 +1,10 @@
+type SearchFormProps = {
+  userName: string;
+  setUserName: React.Dispatch<React.SetStateAction<string>>;
+};
+
+const SearchForm = ({ userName, setUserName }: SearchFormProps) => {
+  return <div>search form</div>;
+};
+
+export default SearchForm;
