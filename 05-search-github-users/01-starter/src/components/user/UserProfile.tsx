02675071f7f24b767027516c1a9b214a6868ab2c
Richard Walsh <richwalsh2@googlemail.com>
2025-08-11 20:13:45 +0100

    section 6 lesson 55

--- 05-search-github-users/01-starter/src/components/user/UserProfile.tsx
+++ 05-search-github-users/01-starter/src/components/user/UserProfile.tsx
@@ -1,9 +1,44 @@
+import { useQuery } from '@apollo/client';
+import { GET_USER } from '@/queries';
+import { type UserData } from '@/types';
+import UserCard from './UserCard';
+import StatsContainer from './StatsContainer';
+
 type UserProfileProps = {
   userName: string;
 };
 
 const UserProfile = ({ userName }: UserProfileProps) => {
-  return <h1 className="text-2xl font-bold">{userName}</h1>;
+  const { data, loading, error } = useQuery<UserData>(GET_USER, {
+    variables: { login: userName },
+  });
+
+  if (loading) return <div>Loading...</div>;
+  if (error) return <h2 className="text-xl">{error.message}</h2>;
+  if (!data) return <h2 className="text-xl">User not found</h2>;
+
+  const {
+    avatarUrl,
+    name,
+    bio,
+    url,
+    repositories,
+    followers,
+    following,
+    gists,
+  } = data.user;
+
+  return (
+    <div>
+      <UserCard avatarUrl={avatarUrl} bio={bio} name={name} url={url} />
+      <StatsContainer
+        totalRepos={repositories.totalCount}
+        followers={followers.totalCount}
+        following={following.totalCount}
+        gists={gists.totalCount}
+      />
+    </div>
+  );
 };
 
 export default UserProfile;
--- 05-search-github-users/01-starter/src/main.tsx
+++ 05-search-github-users/01-starter/src/main.tsx
@@ -3,10 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { Toaster } from './components/ui/toaster.tsx';
+import { ApolloProvider } from '@apollo/client';
+import client from './apolloClient.ts';
+
+// STRICTMODE REMOVED LESSON 53
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-    <Toaster />
+    <ApolloProvider client={client}>
+      <App />
+      <Toaster />
+    </ApolloProvider>
   </StrictMode>
 );
